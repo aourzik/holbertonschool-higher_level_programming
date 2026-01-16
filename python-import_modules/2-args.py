3779d7115894de076a885acb65f77dff56a35694
aourzik <a.ourzik@gmail.com>
2026-01-16 14:39:52 +0100

    feat: create program that prints the number and the list of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 
 args = []
 
-if not args:
-    print(f"{len(args)} : arguments.")
-else:
-    for i in range(len(args)):
-        if len(args) == 1:
-            print(f"{len(args)} : argument :")
-            print(f"{i} : {args[i]}")
-        else:
-            print(f"{len(args)} : arguments :")
-            print(f"{i} : {args[i]}")
+if __name__ == "__main__":
+    if not args:
+        print(f"{len(args)} : arguments.")
+    else:
+        for i in range(len(args)):
+            if len(args) == 1:
+                print(f"{len(args)} : argument :")
+                print(f"{i} : {args[i]}")
+            else:
+                print(f"{len(args)} : arguments :")
+                print(f"{i} : {args[i]}")
