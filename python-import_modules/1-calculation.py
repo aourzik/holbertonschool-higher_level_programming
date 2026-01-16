38b5c51dda9b06b44a4b3007e7b5e8c67a124b09
aourzik <a.ourzik@gmail.com>
2026-01-16 12:08:40 +0100

    fix: modify the first line because i made a mistake on it

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,11 +4,8 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-if __name__ == "__add__":
+if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
-if __name__ == "__sub__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
-if __name__ == "__mul__":
     print("{} * {} = {}".format(a, b, mul(a, b)))
-if __name__ == "__div__":
     print("{} / {} = {}".format(a, b, div(a, b)))
