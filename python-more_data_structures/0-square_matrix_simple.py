73d4a476de124eb30eb8c20e31ec46bbb3bac4b0
aourzik <a.ourzik@gmail.com>
2026-01-20 10:55:14 +0100

    feat: write function that computes the square value of all integers of a matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for line in matrix:
+        new_line = list(map(lambda x: x * x, line))
+        new_matrix.append(new_line)
+
+    return new_matrix
