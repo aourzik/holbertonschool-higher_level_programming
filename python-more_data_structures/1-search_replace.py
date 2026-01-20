62bd26bcf9cc2e33e7aa0723fe6e101696c5ddb2
aourzik <a.ourzik@gmail.com>
2026-01-20 11:02:08 +0100

    fix: add a new variable in function that replaces all occurences of an element by an other in a new list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    return my_list.replace(search, replace)
+    new_list = my_list.replace(search, replace)
+    return new_list
