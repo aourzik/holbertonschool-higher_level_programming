649d76ad7a9c198b0e1b61d9add77b63a45efbab
aourzik <a.ourzik@gmail.com>
2026-01-21 14:54:24 +0100

    feat: write function that prints elements of a list controlling exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        print("{:d}".format(my_list[x]))
-    except IndexError:
-        return 0
+    x_count = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            x_count += 1
+        except IndexError:
+            break
+    print("")
+    return x_count
