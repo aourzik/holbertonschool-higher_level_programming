8322a98ce2e8f61bfaa868977a41cb6cb690401b
aourzik <a.ourzik@gmail.com>
2026-01-21 15:00:40 +0100

    fix: modify name of a variable

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    x_count = 0
-
+    count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            x_count += 1
+            print(my_list[i], end="")
+            count += 1
         except IndexError:
             break
-    print("")
-    return x_count
+    print()
+    return count
