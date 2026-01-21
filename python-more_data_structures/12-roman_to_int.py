25dfc7788c6bd3aabeabcab8108e846b638b7d84
aourzik <a.ourzik@gmail.com>
2026-01-21 13:39:27 +0100

    fix: modify function with te substract condition because i forgot a sign

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 roman = {
-    "I" : 1,
-    "V" : 5,
-    "X" : 10,
-    "L" : 50,
-    "C" : 100,
-    "D" : 500,
-    "M" : 1000
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
 }
+
 
 def roman_to_int(roman_string):
     total = 0
@@ -25,4 +26,8 @@ def roman_to_int(roman_string):
                 total += current_roman
         else:
             total += current_roman
+
+    if i not in roman_string:
+        return None
+
     return total
