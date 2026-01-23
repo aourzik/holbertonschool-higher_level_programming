ca5f2fe929c5f3e78fa42610cc171e56b5d7875b
aourzik <a.ourzik@gmail.com>
2026-01-23 11:34:50 +0100

    feat: write function that divides elements of a matrix and test it

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,43 @@
+#!usr/bin/python3
+"""Module that divides all elements of a Matrix."""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divide the elements of a matrix.
+
+    All elements must be integers or floats, otherwise
+    it raises a TypeError exception.
+
+    Return a matrix of the elements divided. 
+    """
+    if (not isinstance(matrix, list)
+            or matrix == []
+            or not all(isinstance(row, list) for row in matrix)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
+
+    len_row = len(matrix[0])
+    for row in matrix:
+        if len(row) != len_row:
+            raise TypeError(
+                "Each row of the matrix must have the same size"
+            )
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(
+                    "matrix must be a matrix "
+                    "(list of lists) of integers/floats"
+                )
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [
+        [round(element / div, 2) for element in row]
+        for row in matrix
+    ]
