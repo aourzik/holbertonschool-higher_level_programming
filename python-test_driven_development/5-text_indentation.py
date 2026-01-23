e911cf394178a4a93fdc50a19e0876cf1b57da10
aourzik <a.ourzik@gmail.com>
2026-01-23 14:24:06 +0100

    feat: write function that print a sentence with two new lines when there are specials signs, with tests

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module prints a text with 2 new lines after specials characters."""
+
+
+def text_indentation(text):
+    """
+    Docstring for text_indentation function.
+
+    Text: string variable.
+
+    Text must be a string otherwise raises TypeError with an
+    explicit message. There should be no space between or at
+    the end of each printed line.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for _ in text:
+        if _ == "." or "?" or ":":
+            return ("\n" * 2)
+    print("{}".format(text))
