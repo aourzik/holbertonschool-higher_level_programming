adc94d25836edd83fce94914b503f8fab5f4eea8
aourzik <a.ourzik@gmail.com>
2026-01-27 11:12:22 +0100

    feat: create a class that defines a square with a private instance and it's exceptions

---
+++ python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+        if self.__size is not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
