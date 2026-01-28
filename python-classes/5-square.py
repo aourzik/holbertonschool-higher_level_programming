4c2c695c494883c671c63c0c19a49947974bf5f8
aourzik <a.ourzik@gmail.com>
2026-01-28 14:56:59 +0100

    feat: create function that prints a square using its size value

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a square class."""
+
+
+class Square:
+    """ Square defines a square with private instance named size."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a size argument."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size of Square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size of Square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
+    
+    def my_print(self):
+        """Prints the square."""
+        if self.__size == 0:
+            print("")
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
