3d16a37175854bde3a7a82ea38830cae9d72b620
aourzik <a.ourzik@gmail.com>
2026-01-28 16:41:47 +0100

    feat: in rectangle class, create the property for tht wedth and height of the rectangle and the setter property too

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module defines a classe named Rectangle."""
+
+
+class Rectangle:
+    """Rectangle defines a rectangle."""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Return the width of Rectangle."""
+        return self.__width
+
+    @property
+    def height(self):
+        """Return the height of Rectangle."""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width of Rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.widht = value
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height of Rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.height = value
