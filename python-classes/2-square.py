218438aa5fa5a79f0bce4561c279b599d87cf0ea
aourzik <a.ourzik@gmail.com>
2026-01-28 14:23:26 +0100

    fix: modify function to return the good message

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Initialize the square with a size argument."""
 
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
