fda1fa91944b604340e4c3a055e892409c9be4b7
aourzik <a.ourzik@gmail.com>
2026-01-28 14:11:51 +0100

    fix: modify comment to be exact with the role of function

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""This module defines a class who defines a square."""
+
 
 class Square:
+    """
+    Docstring for class Square.
+
+    @size: private instance attribute
+    The square is initialize with a size, an int, called size.
+    """
     def __init__(self, size):
         self._size = size
