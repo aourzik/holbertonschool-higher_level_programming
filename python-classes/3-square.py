c58c586e3c3a3adced00e2a13160ba0df77e6c14
aourzik <a.ourzik@gmail.com>
2026-01-28 14:24:51 +0100

    fix: modify comment and the function to return the good message

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""This module defines a square class."""
+
 
 class Square:
+    """ Square defines a square with private instance named size."""
+
     def __init__(self, size=0):
+        """Initialize the square with a size argument."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
-        if self.__size is not isinstance(self.__size, int):
-            raise TypeError("size must be an integer")
-        elif self.__size < 0:
-            raise ValueError("size must be >= 0")
-        
     def area(self):
         return self.__size
