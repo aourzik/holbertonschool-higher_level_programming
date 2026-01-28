82967611f0677b33a6d1496c3578e8b9d3540966
aourzik <a.ourzik@gmail.com>
2026-01-28 14:51:56 +0100

    fix: modify the function including setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
+"""This module defines a square class."""
+
 
 class Square:
-    def __init__(self, size=0):
-        self.__size = size
+    """ Square defines a square with private instance named size."""
 
-        if self.__size is not isinstance(self.__size, int):
-            raise TypeError("size must be an integer")
-        elif self.__size < 0:
-            raise ValueError("size must be >= 0")
-        
-    def area(self):
+    def __init__(self, size=0):
+        """Initialize the square with a size argument."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size of Square."""
         return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size of Square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
