c3e8e2b0414d4993a88bdb4258b3be10ff9541a4
aourzik <a.ourzik@gmail.com>
2026-01-29 14:19:24 +0100

    fix: put my attribute just after the class before the initiation to be a class public attribute

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -3,8 +3,9 @@
 
 
 class Rectangle:
-    number_of_instances = 0
     """Rectangle defines a rectangle."""
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
         self.width = width
