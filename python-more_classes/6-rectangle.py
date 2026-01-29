111146ae27c34da528deace7edfda87a85209158
aourzik <a.ourzik@gmail.com>
2026-01-29 14:14:05 +0100

    feat: adding a public attribute that is incrimented when instance instanciation and decremented when instance decrementation

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Rectangle defines a rectangle."""
-    def __init__(self, width=0, height=0, number_of_instances=0):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         self.number_of_instances = 0
