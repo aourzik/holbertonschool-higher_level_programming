83c369cd70317e9427acd0199a68e524f7b1b92d
aourzik <a.ourzik@gmail.com>
2026-01-29 15:17:38 +0100

    feat: adding a public class attribute beeing the symbol to print for rectangle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
         self.number_of_instances += 1
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join([str(Rectangle.print_symbol) * self.width for _ in range(self.height)])
+        return "\n".join([str(self.print_symbol) * self.width for _ in range(self.height)])
 
     def __repr__(self):
         """String representation of Rectangle."""
