6246eba0944e26030efcf79de4948c0808244d18
aourzik <a.ourzik@gmail.com>
2026-01-30 16:31:05 +0100

    feat: Create a class method that create a square rectangle

---
+++ python-more_classes/9-rectangle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""This module defines a class named Rectangle."""
+
+
+class Rectangle:
+    """Rectangle defines a rectangle."""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Return the width of Rectangle."""
+        return self.__width
+
+    @property
+    def height(self):
+        """Return the height of Rectangle."""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width of Rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height of Rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of Rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Return the perimeter of Rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Prints the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        lines = []
+        for _ in range(self.__height):
+            lines.append(str(self.print_symbol) * self.__width)
+        return "\n".join(lines)
+
+    def __repr__(self):
+        """String representation of Rectangle."""
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """Message when instance is deleted."""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """Class method that creats a new square Rectangle."""
+        return cls(size, size)
