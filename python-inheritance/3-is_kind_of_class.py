eb1683e56ca044464bfa6aea0a3c6be9f2058b30
aourzik <a.ourzik@gmail.com>
2026-02-03 15:08:26 +0100

    fix: modify function to be smaller

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,7 +3,4 @@
 def is_kind_of_class(obj, a_class):
     """Returns true if object is exactly an instance of the specific class
     or parent class."""
-    if isinstance(obj, a_class) or isinstance(obj, issubclass(a_class)):
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class)
