6eea728ff99ff12a8cfb41249bdf5455377af9ae
aourzik <a.ourzik@gmail.com>
2026-02-03 10:27:54 +0100

    feat: write a class that inherits from a function and prints a sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a class called MyList"""
+
+
+class MyList:
+    """MyList inherits from list"""
+    def print_sorted(self):
+        """Prints the sorted list"""
+        print(sorted(self))
