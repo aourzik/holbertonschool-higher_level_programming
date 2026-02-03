0468af8e3bc87250a17ddc47ffa362c492a0397f
aourzik <a.ourzik@gmail.com>
2026-02-03 15:27:16 +0100

    feat: write a class with a public instance method called area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""This module defines an empty class"""
+
+
+class BaseGeometry:
+    """Defines the area() methods"""
+    def area(self):
+        """Raises an exception because it's not implemented"""
+        raise Exception("area() is not implemented")
