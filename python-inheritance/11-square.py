f428aa4cd8e79b07b544ff183e8004cbcc534b49
aourzik <a.ourzik@gmail.com>
2026-02-04 15:14:13 +0100

    feat: implement the square class with a str methods that returns the description of the square when it's print

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module defines the square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class that inherits from Rectangle class"""
+
+    def __init__(self, size):
+        """Initialize the Square class"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Defines the area of Square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the square description"""
+        return f"[Square] {self.__size}/{self.__size}"
