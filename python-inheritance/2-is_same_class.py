b5d0f2b698e840de7858f195d28a8f310d9f809f
aourzik <a.ourzik@gmail.com>
2026-02-04 10:41:12 +0100

    fix: add the firts docstring sentence describing the module

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This module defines is_the_same function"""
+
 
 def is_same_class(obj, a_class):
     """Returns true if object is exactly an instance of the specific class"""
