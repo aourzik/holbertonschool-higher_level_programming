d213c66d4ae7829566a2cf05a6cade0e0fdc002a
aourzik <a.ourzik@gmail.com>
2026-02-04 14:46:21 +0100

    feat: create a class rectangle that inherits from an other class and defines there privates instances that are validated integers by a methods in the parent class

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines the Rectangle class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class that inherits from BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """Initialize the Rectangle class with width and height arguments"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
