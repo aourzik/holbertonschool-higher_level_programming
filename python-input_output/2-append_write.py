7363a5d261a056f266cc753251b75febb4aeda43
aourzik <a.ourzik@gmail.com>
2026-02-11 10:47:55 +0100

    fix: add docstring to the file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a function that appends a string
+to a text file and returns the number of characters added"""
+
 
 def append_write(filename="", text=""):
-    with open(filename, "a") as f:
+    """Function that appends a string to a UTF-8 text file
+    and returns the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
