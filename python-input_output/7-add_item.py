1568291c9a06d90ab588d7c123b4a9618217d322
aourzik <a.ourzik@gmail.com>
2026-02-11 11:48:11 +0100

    fix: add docstring to the file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a script that adds all arguments
-to a Python list and saves them to a file"""
+"""
+Script that adds all arguments to a Python list, and then saves them
+to a JSON file (add_item.json).
+"""
 
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 filename = "add_item.json"
 
 try:
     items = load_from_json_file(filename)
 except Exception:
-    item = []
+    items = []
 
 items.extend(sys.argv[1:])
 
