732a521f0f87b1222ba68f6cf440ef4bcbab2b74
aourzik <a.ourzik@gmail.com>
2026-02-11 10:03:13 +0100

    fix: add docstring to the file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a function that reads a text file
+and prints it to stdout."""
+
 
 def read_file(filename=""):
+    """Function that reads a text file and prints it to stdout"""
     with open(filename, "r") as f:
         file = f.read()
         print(file)
