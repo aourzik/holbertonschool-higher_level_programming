3d3392d5f705fd28b2bba5d10af33ff73ac2675c
aourzik <a.ourzik@gmail.com>
2026-02-11 11:02:24 +0100

    fix: add docstring to the file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""This module defines a function that creates an object
+from JSON file"""
+
 import json
 
+
 def load_from_json_file(filename):
+    """Function that creates an object from a JSON file"""
     with open(filename, "r") as f:
         return json.load(f)
