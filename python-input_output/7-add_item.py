b53b1db97bfa9ee7a10f6d7283b680af2dba3b86
aourzik <a.ourzik@gmail.com>
2026-02-11 11:06:05 +0100

    fix: add docstring to the file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""This module defines a script that adds all arguments
+to a Python list and saves them to a file"""
+
 import sys, json
 
+
 def save_to_json_file(my_obj, filename):
+    """Function that writes an object to a text file
+    using JSON representation"""
     with open(filename, "w") as f:
         json.dump(my_obj, f)
 
 def load_from_json_file(filename):
+    """Function that creates an object from a JSON file"""
     with open(filename, "r") as f:
         return json.load(f)
 
