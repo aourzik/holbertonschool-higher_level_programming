c1253fc4a5e3e234073ad91bdc1855261da6139a
aourzik <a.ourzik@gmail.com>
2026-02-11 11:12:42 +0100

    fix: add docstring to the file

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""This module defines a function that returns the dictionary
+description with simple data structure for JSON serialization
+of an object"""
+
 
 def class_to_json(obj):
+    """Defines function that returns the dictionary"""
     return obj.__dict__
