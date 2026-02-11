d13923f99b50f85c8365f5136cdd5892e37ffb6b
aourzik <a.ourzik@gmail.com>
2026-02-11 11:46:10 +0100

    fix: modify the import system to import the two functions i need

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,19 +2,11 @@
 """This module defines a script that adds all arguments
 to a Python list and saves them to a file"""
 
-import sys, json
+import sys
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """Function that writes an object to a text file
-    using JSON representation"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
-
-def load_from_json_file(filename):
-    """Function that creates an object from a JSON file"""
-    with open(filename, "r") as f:
-        return json.load(f)
 
 filename = "add_item.json"
 
@@ -24,4 +16,5 @@ except FileNotFoundError:
     item = []
 
 items.extend(sys.argv[1:])
+
 save_to_json_file(items, filename)
