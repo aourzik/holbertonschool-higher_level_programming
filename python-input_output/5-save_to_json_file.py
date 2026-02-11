2bf052049a25fbc27b7ad9c6b648abdb5481cb4f
aourzik <a.ourzik@gmail.com>
2026-02-11 10:59:00 +0100

    fix: add docstring to the file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,4 +9,4 @@ def save_to_json_file(my_obj, filename):
     """Function that writes an object to a text file
     using JSON representation"""
     with open(filename, "w") as f:
-        json.dumps(my_obj, f)
+        json.dump(my_obj, f)
