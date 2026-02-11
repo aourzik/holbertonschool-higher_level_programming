7b3bd8d9c5b0eb257c9b2c4ef87255c76602a03b
aourzik <a.ourzik@gmail.com>
2026-02-11 10:55:19 +0100

    fix: add docstring to the file

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,12 @@
 #/usr/bin/python3
+"""Module that defines a function that returns a Python
+object from JSON"""
+
 import json
 
+
 def from_json_string(my_str):
+    """Function that returns a Python object from a JSON
+    string"""
     with open(my_str) as f:
-        return json.load(f)
+        return json.loads(f)
