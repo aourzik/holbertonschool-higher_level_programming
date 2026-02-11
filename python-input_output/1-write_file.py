bb680199d7473ad150cc741146bcce7bb28c9206
aourzik <a.ourzik@gmail.com>
2026-02-11 10:42:48 +0100

    fix: add docstring to the file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a function that writes a string to
+a text file and returns the number of characters"""
+
 
 def write_file(filename="", text=""):
-    with open(filename, "w") as f:
+    """Function that writes a string to UTF-8 text file and
+    returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
