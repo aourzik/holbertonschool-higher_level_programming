9166aa443a43cd191a17b7cd68b94a3e5076489b
aourzik <a.ourzik@gmail.com>
2026-02-11 11:18:45 +0100

    fix: add docstring to the file

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+"""Module that defines a class that defines a student"""
 
-class student:
+
+class Student:
+    """Defines a student by public instances"""
+
     def __init__(self, first_name, last_name, age):
+        """Defines the instances of student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Public method that retreives a dictionary
+        representation of a student instance"""
         if attrs is None or type(attrs) is not list:
             return self.__dict__
-        
+
         filtered = {}
         for key in attrs:
             if key in self.__dict__:
