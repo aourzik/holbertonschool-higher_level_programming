2cf27c14cf0b34c731240d33d32a00f2a4c3cb5c
aourzik <a.ourzik@gmail.com>
2026-02-17 15:27:59 +0100

    feat: finish the script with the two functions that structure and print api datas in csv format

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,16 +3,17 @@
 import requests
 import csv
 
-def fetch_and_print_post():
+def fetch_and_print_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
     print(f"Status Code: {response.status_code}")
     
     if response.status_code == 200:
         posts = response.json()
-        print([post['title'] for post in posts])
+        for post in posts:
+            print(post['title'])
     else:
         return [] 
-fetch_and_print_post()
+fetch_and_print_posts()
 
 def fetch_and_save_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
@@ -27,4 +28,9 @@ def fetch_and_save_posts():
                 "body": post['body']
             })
         print(structured_data)
+    
+        with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+            final_file = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            final_file.writeheader()
+            final_file.writerows(structured_data)
 fetch_and_save_posts()
