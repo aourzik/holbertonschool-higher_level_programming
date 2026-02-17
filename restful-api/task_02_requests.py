141d56c4afd3a657f182b5e2b1f56bce859c88f3
aourzik <a.ourzik@gmail.com>
2026-02-17 14:45:23 +0100

    feat: second part of the script with second function that structure data of api in json format with the keys names

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import requests
+import csv
 
 def fetch_and_print_post():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {response.status_code}")
     
     if response.status_code == 200:
         posts = response.json()
@@ -11,3 +13,18 @@ def fetch_and_print_post():
     else:
         return [] 
 fetch_and_print_post()
+
+def fetch_and_save_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    if response.status_code == 200:
+        posts = response.json()
+        structured_data = []
+        for post in posts:
+            structured_data.append({
+                "id": post['id'],
+                "title": post['title'],
+                "body": post['body']
+            })
+        print(structured_data)
+fetch_and_save_posts()
