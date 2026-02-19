cc6a403c3e2118fa56691f5506513a300f7b5e32
aourzik <a.ourzik@gmail.com>
2026-02-19 19:20:00 +0100

    feat: create a flask app an run a develop server

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {}
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/data")
+def get_usernames():
+    return jsonify(list(users.keys()))
+
+@app.route("/users/<username>")
+def get_user(username):
+    if username not in users:
+        return jsonify({"error": "user not found"}), 404
+    return jsonify(users[username])
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    if not request.is_json:
+        return jsonify({"error": "Invalid JSON"}), 400
+    
+    data = request.get_json()
+
+    if "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+    
+    username = data["username"]
+
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 409
+    
+    users[username] = data
+
+    return jsonify({"message": "added", "user": data}), 201
+
+if __name__ == "__main__":app.run(host="0.0.0.0", port=5000)
