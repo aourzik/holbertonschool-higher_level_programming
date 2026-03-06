5b9538e4c83ba82fb0fd036b74a941e075677be0
aourzik <a.ourzik@gmail.com>
2026-03-06 17:28:43 +0100

    fix: modify the code to be pycodestyle friendly

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
+"""Link the State class to the MySQL table states."""
+
 import sys
+
+from sqlalchemy import create_engine
 from model_state import Base, State
 
-from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
     Base.metadata.create_all(engine)
