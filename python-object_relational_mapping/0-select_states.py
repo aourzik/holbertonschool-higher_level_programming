d0571d3f0991cb0aa3e223a548e7a16405e15620
aourzik <a.ourzik@gmail.com>
2026-03-06 14:59:36 +0100

    fix: modify script to sort in ascending order by states.id

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -20,11 +20,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-    rows = cursor.fetchall()
-
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
 
     cursor.close()
