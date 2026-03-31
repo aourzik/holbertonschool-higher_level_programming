e0c8d3ecba7fffc86d1a05e9ef4e97f7a9a62437
aourzik <a.ourzik@gmail.com>
2026-03-31 17:04:58 +0200

    fix: erase the controler to see if there is no arg

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,8 +2,4 @@
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-if (arg1 === undefined || arg2 === undefined) {
-  console.log('undefined is undefined');
-} else {
-  console.log(arg1 + ' is ' + arg2);
-}
+console.log(arg1 + ' is ' + arg2);
