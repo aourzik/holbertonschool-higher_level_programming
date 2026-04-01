5a00c685b4d67ae92f979f2291e0a830ccca8c16
aourzik <a.ourzik@gmail.com>
2026-04-01 17:04:16 +0200

    feat: Write a JavaScript script that toggles the class of the header element when the user clicks on the tag id toggle_header

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,8 @@
+#!/bin/usr/node
+const toggleHeader = document.querySelector('#toggle_header');
+const header = document.querySelector('header');
+
+toggleHeader.addEventListener('click', function () {
+  header.classList.toggle('red');
+  header.classList.toggle('green');
+});
