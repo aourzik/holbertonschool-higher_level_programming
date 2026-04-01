7ecb0e2c6dc79256217f0f27ea901877ec033570
aourzik <a.ourzik@gmail.com>
2026-04-01 17:51:34 +0200

    feat: Write a JavaScript script that fetches from https://hellosalut.stefanbohacek.com/?lang=fr and displays the value of hello from that fetch in the HTML element with id hello.

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,10 @@
+#!/bin/usr/node
+document.addEventListener('DOMContentLoaded', function () {
+  const helloDiv = document.querySelector('#hello');
+
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      helloDiv.textContent = data.hello;
+    });
+});
