2b34aa33146adb0fca9e30458154dfd25d9047cf
aourzik <a.ourzik@gmail.com>
2026-04-01 10:00:49 +0200

    feat: change header text color on click

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+#!/bin/usr/node
+const clickColorHeader = document.querySelector('header');
+
+clickColorHeader.addEventListener('click', function () {
+  clickColorHeader.style.color = '#FF0000';
+});
