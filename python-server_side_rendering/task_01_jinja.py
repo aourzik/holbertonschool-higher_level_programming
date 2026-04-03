ebd342b668a7168c07f99b199766f44defe956a3
aourzik <a.ourzik@gmail.com>
2026-04-03 11:07:37 +0200

    feat: create  basic flask application that serves a web page using a jinja template

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,20 @@
+#!/usr/bin/venv python3
+"""Module that uses Jinja2 to generate HTML files from a template and a list of attendees"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+       return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+       return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
