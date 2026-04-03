5012cccd4356a51748f04598f5d336ed25d00820
aourzik <a.ourzik@gmail.com>
2026-04-03 14:20:44 +0200

    feat: a feature that displays products in csv or json format

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/venv python3
 """Module that uses Jinja2 to generate HTML files from a template and a list of attendees"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 import csv
 
@@ -33,8 +33,38 @@ def read_json(filename):
 @app.route('/products')
 def products():
     """Route that displays a list of products from a CSV file or JSON file"""
-    products = read_csv('products.csv') or read_json('products.json')
-    return render_template('products.html', products=products.get('products', []))
+    source = request.args.get('source')
+    products_id = request.args.get('id')
+    if source == 'csv':
+        products_dict = read_csv('products.csv')
+    elif source == 'json':
+        products_dict = read_json('products.json')
+    else:
+        return render_template('products.html',
+                               error='Invalid source specified',
+                               products=[])
+    
+    if products_id is not None:
+        try:            
+            products_id = int(products_id)
+        except ValueError:
+            return render_template('products.html',
+                                   error='Invalid product ID specified',
+                                   products=[])
+    
+        filtered_products = []
+        for product in products_dict:
+            if products_id is None or product['id'] == products_id:
+                filtered_products.append(product)
+    
+        if not filtered_products:
+            return render_template('products.html',
+                                error='No products found with the specified criteria',
+                                products=[])
+    
+        return render_template('products.html', products=filtered_products)
+    
+    return render_template('products.html', products=products_dict) 
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
