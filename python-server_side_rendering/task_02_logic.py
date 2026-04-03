6bb7396d1007ed0e37974a67f5e1e02e892ed8e4
aourzik <a.ourzik@gmail.com>
2026-04-03 12:17:37 +0200

    fix: modify my flask app because it won't get my json

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -21,7 +21,7 @@ def contact():
 def items():
     with open('items.json') as f:
         items = json.load(f)
-    return render_template('items.html', items=items)
+    return render_template('items.html', items=items.get('items', []))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
