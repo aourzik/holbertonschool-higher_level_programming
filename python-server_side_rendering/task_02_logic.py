ffabda3a4a28c9a54786ada289c3a8f0b8d4fcd9
aourzik <a.ourzik@gmail.com>
2026-04-03 12:05:34 +0200

    feat: create Flask application by integrating dynamic content into your HTML templates using Jinja's loop and conditional constructs

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,27 @@
+#!/usr/bin/venv python3
+"""Module that uses Jinja2 to generate HTML files from a template and a list of attendees"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+       return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+       return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open('items.json') as f:
+        data = f.read()
+    items = eval(data)['items']
+    return render_template('items.html', items=items)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
